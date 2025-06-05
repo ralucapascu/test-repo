39bc34d013818de7e0752432aa26ad40759b9c0c
Raluca Pascu <ralucaioana2000@gmail.com>
2025-06-05 13:29:20 +0300

    pushed a new fix

--- src/main/java/com/devscan/taskmanager/exception/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.devscan.taskmanager.exception;
-
-public class TaskNotFoundException extends RuntimeException {
-    public TaskNotFoundException(String message) {
-        super(message);
-    }
-}
